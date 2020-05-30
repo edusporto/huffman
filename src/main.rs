70101080fbcf716f062b5c14e32efd59cc63ffc3
Eduardo Sandalo Porto <eduardosandaloporto@gmail.com>
2020-05-30 18:10:45 -0300

    Minor refactor in main.rs to improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 use std::error::Error;
 use std::fs::File;
@@ -46,37 +46,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut content = Vec::new();
     f.read_to_end(&mut content)?;
 
-    if !matches.is_present("decompress") {
-        let threads = match matches.value_of("threads") {
-            Some(t) => {
-                let t = t.parse::<usize>();
-                let cpus = num_cpus::get();
-                match t {
-                    Ok(t) => {
-                        if t > cpus {
-                            Err(format!("There are only {} threads avaiable", cpus))
-                        } else if t < 1 {
-                            Err(format!("Unexpected amount of threads {}", t))
-                        } else {
-                            Ok(t)
-                        }
-                    }
-                    Err(e) => Err(format!("Number of threads not valid: {}", e)),
-                }?
-            }
-            None => num_cpus::get(),
-        };
+    match matches.is_present("decompress") {
+        false => {
+            let threads = get_threads(&matches)?;
+            let compressed = huffman::compress(&content, threads);
 
-        let compressed = huffman::compress(&content, threads);
-        println!("Content size: {}", content.len());
-        println!("Compressed size: {}", compressed.len());
-        println!(
-            "Compressed to {:.2}% of original file",
-            compressed.len() as f64 / content.len() as f64 * 100.0
-        );
-    } else {
-        println!("Decompression feature not implemented yet.");
+            println!("Content size: {}", content.len());
+            println!("Compressed size: {}", compressed.len());
+            println!(
+                "Compressed to {:.2}% of original file",
+                compressed.len() as f64 / content.len() as f64 * 100.0
+            );
+        }
+        true => {
+            println!("Decompression feature not implemented yet.");
+        }
     }
 
     Ok(())
+}
+
+pub fn get_threads(matches: &ArgMatches) -> Result<usize, String> {
+    match matches.value_of("threads") {
+        Some(t) => {
+            let t = t.parse::<usize>();
+            let cpus = num_cpus::get();
+            match t {
+                Ok(t) => {
+                    if t > cpus {
+                        Err(format!("There are only {} threads avaiable", cpus))
+                    } else if t < 1 {
+                        Err(format!("Unexpected amount of threads {}", t))
+                    } else {
+                        Ok(t)
+                    }
+                }
+                Err(e) => Err(format!("Number of threads not valid: {}", e)),
+            }
+        }
+        None => Ok(num_cpus::get()),
+    }
 }
